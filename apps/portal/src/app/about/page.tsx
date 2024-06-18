a1ea5194dc314245bda9c6d4a94aaa7dea9dcef0
ni.bagherpour <ni.bagherpour@sadad.co.ir>
2024-06-18 10:30:45 +0330

    feat: add about page

--- apps/portal/src/app/about/page.tsx
+++ apps/portal/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+
+// import homePic from '../../../public/home.png'
+
 export default async function About() {
 
   const response = await fetch("https://api.adviceslip.com/advice");
@@ -7,6 +11,12 @@ export default async function About() {
     <main>
       <h1>About Page</h1>
       <p>{data.slip.advice}</p>
+      <Image
+      src={'/home.png'} // Replace with your image filename
+      alt="Description of your image"
+      width={300} // Optional: Set image width
+      height={300} // Optional: Set image height
+    />
     </main>
   );
 }
